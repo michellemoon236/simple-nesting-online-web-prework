47f78568fb940ff53b5533a1adf7bb10648d1142
Michelle M <michellemoon236@gmail.com>
2019-03-23 06:33:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,8 +65,8 @@ def adding_matz
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
-programmer_hash[]
-# }
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+end
 
 	programmer_hash = 
  		{
